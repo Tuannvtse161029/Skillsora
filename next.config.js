dc7c795269bd920d94dffbf555bc36425b1939ad
Tuannvtse161029 <tuannvtse161029@fpt.edu.vn>
2026-02-04 22:09:45 +0700

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: false,
-  distDir: 'build',
-  output: 'standalone',
   images: {
     domains: [
       "web-img.rensa.jp.net",
@@ -10,24 +8,12 @@ const config = {
       "owburyfqlnlvzvwkadfp.supabase.co"
     ],
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'psszjkdspnyifyjbmyau.supabase.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'bcp.cdnchinhphu.vn',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images2.thanhnien.vn',
-      },
-      {
-        protocol: 'https',
-        hostname: 'toongadventure.vn',
-      },
+      { protocol: 'https', hostname: 'psszjkdspnyifyjbmyau.supabase.co' },
+      { protocol: 'https', hostname: 'bcp.cdnchinhphu.vn' },
+      { protocol: 'https', hostname: 'images2.thanhnien.vn' },
+      { protocol: 'https', hostname: 'toongadventure.vn' },
     ],
   },
-};
+}
 
-module.exports = config;
+module.exports = config
